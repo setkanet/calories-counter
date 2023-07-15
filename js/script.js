e4666c956ff4e4ab3377b70ed4505b5b3ea505af
setkanet <128897001+setkanet@users.noreply.github.com>
2023-07-15 12:10:55 +0300

    Изменил JS

--- js/script.js
+++
@@ -1,8 +0,0 @@
-import Counter from './modules/counter.js';
-
-const counterElements = document.querySelectorAll(`.counter`);
-
-counterElements.forEach((element) => {
-  const counter = new Counter(element);
-  counter.init();
-});
--- js/utils/format-number.js
+++
@@ -1,3 +0,0 @@
-export const formatNumber = (num) => {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, `$1 `);
-};
